2c9e733635ee41ce74ec77637eb77762b6366cdd
dogukanaslan <92223153+dogukanaslan@users.noreply.github.com>
2022-09-11 13:42:37 +0300

    commit

--- src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Models.Models;
 using Application.Features.Models.Queries.GetListModel;
+using Application.Features.Models.Queries.GetListModelByDynamic;
 using Core.Application.Requests;
+using Core.Persistence.Dynamic;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +19,15 @@ namespace WebAPI.Controllers
             GetListModelQuery getListModelQuery = new GetListModelQuery() { PageRequest = pageRequest}; 
 
             ModelListModel result = await Mediator!.Send(getListModelQuery);
+            return Ok(result);
+        }
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<ActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListModelByDynamicQuery getListModelByDynamicQuery = new GetListModelByDynamicQuery() { PageRequest = pageRequest, Dynamic = dynamic };
+
+            ModelListModel result = await Mediator!.Send(getListModelByDynamicQuery);
             return Ok(result);
         }
     }
